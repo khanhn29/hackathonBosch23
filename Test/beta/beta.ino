9a630d51c561b657d30eab82f51ce425e2a3e88f
khanhn29 <khanh95hd@gmail.com>
2023-09-14 00:26:38 +0700

    update

--- Test/beta/beta.ino
+++ Test/beta/beta.ino
@@ -8,6 +8,14 @@ void setup() {
   Serial.println("16 channel Servo test!");
   pwm.begin();
   pwm.setPWMFreq(60); 
+  controlShoulderFR(CLOSE_ANGLE_SHOULDER_FR);
+  controlShoulderFL(CLOSE_ANGLE_SHOULDER_FL);
+  controlShoulderRR(CLOSE_ANGLE_SHOULDER_RR);
+  controlShoulderRL(CLOSE_ANGLE_SHOULDER_RL);
+
+  controlArmFR(CLOSE_ANGLE_ARM_FR);
+  controlArmRL(CLOSE_ANGLE_ARM_RL);
+  controlArmFL(CLOSE_ANGLE_ARM_FL);
 }
 void loop() {
 
@@ -40,22 +48,37 @@ void loop() {
   // controlShoulderRL(OPEN_ANGLE_SHOULDER_RL);
   // delay(2000);
 
-  controlShoulderFL(MIDDLE_ANGLE_SHOULDER_FL);
-  controlShoulderFR(OPEN_ANGLE_SHOULDER_FR);
-  controlShoulderRL(MIDDLE_ANGLE_SHOULDER_RL);
-  controlShoulderRR(OPEN_ANGLE_SHOULDER_RR);
+  // controlShoulderFL(MIDDLE_ANGLE_SHOULDER_FL);
+  // controlShoulderFR(OPEN_ANGLE_SHOULDER_FR);
+  // controlShoulderRL(MIDDLE_ANGLE_SHOULDER_RL);
+  // controlShoulderRR(OPEN_ANGLE_SHOULDER_RR);
+  // delay(1000);
+  // controlShoulderFR(MIDDLE_ANGLE_SHOULDER_FR);
+  // controlShoulderFL(OPEN_ANGLE_SHOULDER_FL);
+  // controlShoulderRR(MIDDLE_ANGLE_SHOULDER_RR);
+  // controlShoulderRL(OPEN_ANGLE_SHOULDER_RL);
+  // delay(1000);
+  // controlArmFR(CLOSE_ANGLE_ARM_FR);
+  // controlArmRL(CLOSE_ANGLE_ARM_RL);
+  // controlArmFL(CLOSE_ANGLE_ARM_FL);
+  controlArmRR(CLOSE_ANGLE_ARM_RR);
+  delay(2000);
+  // controlArmFR(STAND_ANGLE_ARM_FR);
+  // controlArmRL(STAND_ANGLE_ARM_RL);
+  // controlArmFL(STAND_ANGLE_ARM_FL);
+  controlArmRR(STAND_ANGLE_ARM_RR);
   delay(1000);
-  controlShoulderFR(MIDDLE_ANGLE_SHOULDER_FR);
-  controlShoulderFL(OPEN_ANGLE_SHOULDER_FL);
-  controlShoulderRR(MIDDLE_ANGLE_SHOULDER_RR);
-  controlShoulderRL(OPEN_ANGLE_SHOULDER_RL);
+  // controlArmFR(RAISE_ANGLE_ARM_FR);
+  // controlArmRL(RAISE_ANGLE_ARM_RL);
+  // controlArmFL(RAISE_ANGLE_ARM_FL);
+  controlArmRR(RAISE_ANGLE_ARM_RR);
   delay(1000);
 }
 void controlShoulderFR(int angle)
 {
   if(angle > SAFE_ANGLE_SHOULDER_FR)
     angle = SAFE_ANGLE_SHOULDER_FR;
-  if(angle <= 0)
+  if(angle < 0)
     angle = 0;
   pwm.setPWM(SERVO_NUM_SHOULDER_FR, 0, angleToPulse(angle));
 }
@@ -64,7 +87,7 @@ void controlShoulderFL(int angle)
 {
   if(angle > SAFE_ANGLE_SHOULDER_FL)
     angle = SAFE_ANGLE_SHOULDER_FL;
-  if(angle <= 0)
+  if(angle < 0)
     angle = 0;
   pwm.setPWM(SERVO_NUM_SHOULDER_FL, 0, angleToPulse(angle));
 }
@@ -73,7 +96,7 @@ void controlShoulderRR(int angle)
 {
   if(angle > SAFE_ANGLE_SHOULDER_RR)
     angle = SAFE_ANGLE_SHOULDER_RR;
-  if(angle <= 0)
+  if(angle < 0)
     angle = 0;
   pwm.setPWM(SERVO_NUM_SHOULDER_RR, 0, angleToPulse(angle));
 }
@@ -82,9 +105,45 @@ void controlShoulderRL(int angle)
 {
   if(angle > SAFE_ANGLE_SHOULDER_RL)
     angle = SAFE_ANGLE_SHOULDER_RL;
-  if(angle <= 0)
+  if(angle < 0)
     angle = 0;
   pwm.setPWM(SERVO_NUM_SHOULDER_RL, 0, angleToPulse(angle));
+}
+
+void controlArmFR(int angle)
+{
+  if(angle > SAFE_MAX_ANGLE_ARM_FR)
+    angle = SAFE_MAX_ANGLE_ARM_FR;
+  if(angle < SAFE_MIN_ANGLE_ARM_FR)
+    angle = SAFE_MIN_ANGLE_ARM_FR;
+  pwm.setPWM(SERVO_NUM_ARM_FR, 0, angleToPulse(angle));
+}
+
+void controlArmRL(int angle)
+{
+  if(angle > SAFE_MAX_ANGLE_ARM_RL)
+    angle = SAFE_MAX_ANGLE_ARM_RL;
+  if(angle < SAFE_MIN_ANGLE_ARM_RL)
+    angle = SAFE_MIN_ANGLE_ARM_RL;
+  pwm.setPWM(SERVO_NUM_ARM_RL, 0, angleToPulse(angle));
+}
+
+void controlArmFL(int angle)
+{
+  if(angle > SAFE_MAX_ANGLE_ARM_FL)
+    angle = SAFE_MAX_ANGLE_ARM_FL;
+  if(angle < SAFE_MIN_ANGLE_ARM_FL)
+    angle = SAFE_MIN_ANGLE_ARM_FL;
+  pwm.setPWM(SERVO_NUM_ARM_FL, 0, angleToPulse(angle));
+}
+
+void controlArmRR(int angle)
+{
+  if(angle > SAFE_MAX_ANGLE_ARM_RR)
+    angle = SAFE_MAX_ANGLE_ARM_RR;
+  if(angle < SAFE_MIN_ANGLE_ARM_RR)
+    angle = SAFE_MIN_ANGLE_ARM_RR;
+  pwm.setPWM(SERVO_NUM_ARM_RR, 0, angleToPulse(angle));
 }
 
 int angleToPulse(int ang){
